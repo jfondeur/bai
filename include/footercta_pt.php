50f3a60ddedac4ed028f4b2e5957c32f7e6cd4c0
Jean Fondeur <jean@fondeurs.com>
2018-09-22 11:27:54 -0400

    contact fix

--- include/footercta_pt.php
+++ include/footercta_pt.php
@@ -3,7 +3,7 @@
             <div class="row justify-content-center">
                 <div class="col-8 text-center">
                     <h2 class="brown">Solicita uma chamada com um consultor hoje</h2>
-                    <p>Get started by having a 20 minutes call with us</p>
+                    <p>Nossos consultores apoiam investidores latino-americanos em espanhol e português</p>
                     <?php get_template_part('include/optin')?>
                 </div>
             </div>
