7ef4aa5bab24a1e325456b2e5815e286ab86b1d8
Jean Fondeur <jean@fondeurs.com>
2018-09-26 18:04:26 -0400

    language

--- include/optin.php
+++ include/optin.php
@@ -6,7 +6,9 @@
         $leadsource = esc_url($_COOKIE['leadsource']);
     } elseif(isset($_SERVER['HTTP_REFERER'])){
         $leadsource = $_SERVER['HTTP_REFERER'];
-    }
+    };
+
+    $currentlang = apply_filters( 'wpml_current_language', NULL );
 ?>
 
 <form accept-charset="UTF-8" action="https://qz363.infusionsoft.com/app/form/process/6730495c336af971211de89344250e00" class="infusion-form form-inline" id="inf_form_6730495c336af971211de89344250e00" method="POST">
@@ -20,5 +22,6 @@
     <input name="inf_custom_CampaignContent" type="hidden" value="<?php if (isset($utms)){ echo $utms[4]; }?>" />
     <input name="inf_custom_CampaignMedium" type="hidden" value="<?php if (isset($utms)){ echo $utms[1]; }?>" />
     <input name="inf_custom_URLSource" type="hidden" value="<?php if (isset($leadsource)){ echo $leadsource; }?>" />
+    <input name="inf_custom_WebsiteLanguage" type="hidden" value="<?php if (isset($currentlang)){ echo $currentlang; }?>" />
     <button class="btn btn-primary mb-2" type="submit">Enviar</button>
 </form>
